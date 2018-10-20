e736e094f961c12d1dc0fb737f14f5ce4e04b466
周国宝 <zhou_guobao@qq.com>
2018-10-20 22:02:20 +0800

    release V1

---
+++ app/src/main/java/com/zhou/reader/ShelfActivity.java
@@ -0,0 +1,30 @@
+package com.zhou.reader;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhou.reader.shelf.ShelfFragment;
+
+public class ShelfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shelf);
+        initFragment();
+    }
+
+    private void initFragment() {
+        ShelfFragment shelfFragment = (ShelfFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.contentFrame);
+
+        if (shelfFragment == null) {
+            shelfFragment = new ShelfFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.add(R.id.contentFrame, shelfFragment);
+            transaction.commit();
+        }
+    }
+
+}
