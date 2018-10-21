a6e4e7c70a1b477225f83e7739527be8e42f4a69
周国宝 <zhou_guobao@qq.com>
2018-10-21 11:31:37 +0800

    add fabic crashlytics

--- app/src/main/java/com/zhou/reader/SplashActivity.java
+++ app/src/main/java/com/zhou/reader/SplashActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
+import io.fabric.sdk.android.Fabric;
+
 public class SplashActivity extends Activity {
 
     @Override
@@ -13,6 +18,14 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         mHandler.sendEmptyMessageDelayed(0,1000);
+        initFabric();
+    }
+
+    private void initFabric(){
+        Crashlytics crashlyticsKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+        Fabric.with(this, crashlyticsKit);
     }
 
     Handler mHandler = new Handler(){
